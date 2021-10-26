5fb558a2a1708753ca8bd1a17e524b298b1e2e66
JOÃO HENRIQUE BATISTA CERQUEIRA <joaohbp59@gmail.com>
2021-10-26 18:11:16 -0300

    update controller

---
+++ src/main/java/br/pro/cadastra/cadastro/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package br.pro.cadastra.cadastro.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.pro.cadastra.cadastro.domain.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Byte> {
+
+}

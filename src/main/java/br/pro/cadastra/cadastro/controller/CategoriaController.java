fc50a8e6edb598f03e88a379a12e2f6658250004
JOÃO HENRIQUE BATISTA CERQUEIRA <joaohbp59@gmail.com>
2021-10-27 12:12:23 -0300

    Update CategoriaController.java

--- src/main/java/br/pro/cadastra/cadastro/controller/CategoriaController.java
+++ src/main/java/br/pro/cadastra/cadastro/controller/CategoriaController.java
@@ -1,10 +1,14 @@
 package br.pro.cadastra.cadastro.controller;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,5 +32,20 @@ public class CategoriaController {
 	public Categoria inserir (@RequestBody Categoria categoria) {
 		Categoria categoriaSalva = categoriaRepository.save(categoria);
 		return categoriaSalva;
+	}
+	
+	@DeleteMapping("/{codigo}")
+	public Categoria excluir (@PathVariable Short codigo) {
+		Optional<Categoria> categoria = categoriaRepository.findById(codigo);
+		categoriaRepository.delete(categoria.get());
+		Categoria categoriaRetorno = categoria.get();
+		return categoriaRetorno;
+	}
+	
+	@PutMapping
+	public Categoria editar(@RequestBody Categoria categoria) {
+		Categoria categoriaEditada =  categoriaRepository.save(categoria);
+		return categoriaEditada;
+		
 	}
 }
